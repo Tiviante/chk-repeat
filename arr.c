c6ea958bfe61fce28166260febcf0fcb0bc255ce
Tiviante <95531771+Tiviante@users.noreply.github.com>
2021-12-04 19:50:51 +0700

    Add files via upload

---
+++ arr.c
@@ -0,0 +1,267 @@
+check arr[0];
+if (arr[1] = compound > arr[0])
+{
+    arr[0] - arr[1];
+    if (arr[2] = compound > arr[0])
+    {
+        arr[0] - arr[2];
+        if (arr[3] = compound > arr[0])
+        {
+            arr[0] - arr[3];
+        }
+    }
+}
+
+move > arr[1];
+check arr[1];
+if (arr[0] = compound > arr[1])
+{
+    arr[1] - arr[0];
+    if (arr[2] = compound > arr[1])
+    {
+        arr[1] - arr[2];
+        if (arr[3] = compound > arr[1])
+        {
+            arr[1] - arr[3];
+        }
+    }
+    
+}
+
+move > arr[2];
+check arr[2];
+if (arr[0] = compound > arr[2])
+{
+    arr[2] - arr[0];
+    if (arr[1] = compound > arr[2])
+    {
+        arr[2] - arr[1];
+        if (arr[3] = compound > arr[2])
+        {
+            arr[2] - arr[3];
+        }
+    }
+}
+
+move > arr[3];
+check arr[3];
+if (arr[0] = compound > arr[3])
+{
+    arr[3] - arr[0];
+    if (arr[1] = compound > arr[3])
+    {
+        arr[3] - arr[1];
+        if (arr[2] = compound > arr[3])
+        {
+            arr[3] - arr[2];
+        }
+        
+    }
+    
+}
+
+move > arr[4];
+check arr[4];
+if (arr[5] = compound > arr[4])
+{
+    arr[4] - arr[5];
+    if (arr[6] = compound > arr[4])
+    {
+        arr[4] - arr[6];
+        if (arr[7] = compound > arr[4])
+        {
+            arr[4] - arr[7];
+        }
+        
+    }
+    
+}
+
+move > arr[5];
+check arr[5];
+if (arr[4] = compound > arr[5])
+{
+    arr[5] - arr[4];
+    if (arr[6] = compound > arr[5])
+    {
+        arr[5] - arr[6];
+        if (arr[7] = compound > arr[5])
+        {
+            arr[5] - arr[7];
+        }
+        
+    }
+    
+}
+
+move > arr[6];
+check arr[6];
+if (arr[4] = compound > arr[6])
+{
+    arr[6] - arr[4];
+    if (arr[5] = compound > arr[6])
+    {
+        arr[6] - arr[5];
+        if (arr[7] = compound > arr[6])
+        {
+            arr[6] - arr[7];
+        }
+        
+    }
+    
+}
+
+move > arr[7];
+check arr[7];
+if (arr[4] = compound > arr[7])
+{
+    arr[7] - arr[4];
+    if (arr[5] = compound > arr[7])
+    {
+        arr[7] - arr[5];
+        if (arr[6] = compound > arr[7])
+        {
+            arr[7] - arr[6];
+        }
+        
+    }
+    
+}
+
+move > arr[8];
+check arr[8];
+if (arr[9] = compound > arr[8])
+{
+    arr[8] - arr[9];
+    if (arr[10] = compound > arr[8])
+    {
+        arr[8] - arr[10];
+        if (arr[11] = compound > arr[8])
+        {
+            arr[8] - arr[11];
+        }
+        
+    }
+    
+}
+
+move > arr[9];
+check arr[9];
+if (arr[8] = compound > arr[9])
+{
+    arr[9] - arr[8];
+    if (arr[10] = compound > arr[9])
+    {
+        arr[9] - arr[10];
+        if (arr[11] = compound > arr[9])
+        {
+            arr[9] - arr[11];
+        }
+        
+    }
+    
+}
+
+move > arr[10];
+check arr[10];
+if (arr[8] = compound > arr[10])
+{
+    arr[10] - arr[8];
+    if (arr[9] = compound > arr[10])
+    {
+        arr[10] - arr[9];
+        if (arr[11] = compound > arr[10])
+        {
+            arr[10] - arr[11];
+        }
+        
+    }
+    
+}
+
+move > arr[11];
+check arr[11];
+if (arr[8] = compound > arr[11])
+{
+    arr[11] - arr[8];
+    if (arr[9] = compound > arr[11])
+    {
+        arr[11] - arr[9];
+        if (arr[10] = compound > arr[11])
+        {
+            arr[11] - arr[10];
+        }
+        
+    }
+    
+}
+
+move > arr[12];
+check arr[12];
+if (arr[13] = compound > arr[12])
+{
+    arr[12] - arr[13];
+    if (arr[14] = compound > arr[12])
+    {
+        arr[12] - arr[14];
+        if (arr[15] = compound > arr[12])
+        {
+            arr[12] - arr[15];
+        }
+        
+    }
+    
+}
+
+move > arr[13];
+check arr[13];
+if (arr[12] = compound > arr[13])
+{
+    arr[13] - arr[12];
+    if (arr[14] = compound > arr[13])
+    {
+        arr[13] - arr[14];
+        if (arr[15] = compound > arr[13])
+        {
+            arr[13] - arr[15];
+        }
+        
+    }
+    
+}
+
+move > arr[14];
+check arr[14];
+if (arr[12] = compound > arr[14])
+{
+    arr[14] - arr[12];
+    if (arr[13] = compound > arr[14])
+    {
+        arr[14] - arr[13];
+        if (arr[15] = compound > arr[14])
+        {
+            arr[14] - arr[15];
+        }
+        
+    }
+    
+}
+
+move > arr[15];
+check arr[15];
+if (arr[12] = compound > arr[15])
+{
+    arr[15] - arr[12];
+    if (arr[13] = compound > arr[15])
+    {
+        arr[15] - arr[13];
+        if (arr[14] = compound > arr[15])
+        {
+            arr[15] - arr[14];
+        }
+        
+    }
+    
+}
+
+repeat to arr[0];
